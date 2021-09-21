f9299b3f8594b4001b2637d6e36a063f27e44f3c
zzzsyyy <zsy3141592@163.com>
2021-09-21 15:58:27 +0800

    update

---
+++ src/leetcode/9-20/leetcode-1.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		another := target - nums[i]
+		if _, ok := m[another]; ok {
+			return []int{m[another], i}
+		}
+		m[nums[i]] = i
+	}
+	return nil
+}
+
+func main() {
+	nums := []int{2, 7, 9, 11}
+	ret := twoSum(nums, 13)
+	fmt.Println(ret)
+}
